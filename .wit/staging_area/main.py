5134bc17d1c88bdbbf474c426fd455a3fa3b883e
ShaniShurkin <shanishurkin@gmail.com>
2023-07-12 13:43:17 +0300

    added .wit to git ignore

--- .wit/staging_area/main.py
+++
@@ -1,9 +0,0 @@
-from wit_interface import WitInterface
-import sys
-
-if __name__ == '__main__':
-    command = sys.argv[1]
-    args = sys.argv[2:]
-    WitInterface.handle_commands(command, args)
-
-
