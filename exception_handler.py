3b554ea5edae117bcfdd6e497a8c034ff54000a1
ShaniShurkin <shanishurkin@gmail.com>
2023-07-12 19:42:56 +0300

    exception decorator woks with files

--- exception_handler.py
+++ exception_handler.py
@@ -10,11 +10,9 @@ def exception_handler(func):
     def inner_function(*args, **kwargs):
         try:
             func(*args, **kwargs)
-        except FileExistsError:
-            message = f"The folder {os.path.basename(os.path.normpath(path))} already exists in the path: {path}"
-            err_logger(message)
+        except FileExistsError as ffe:
+            err_logger(ffe)
         except Exception as e:
-            message = f"An error occurred while creating the folder: {str(e)}"
-            err_logger(message)
+            err_logger(e)
 
     return inner_function
--- files_handler.py
+++ files_handler.py
@@ -1,8 +1,11 @@
+import math
 import os
 import shutil
 from exception_handler import exception_handler
 from logger import Logger
 import logging
+
+from wit_exception import WitException
 
 
 class FileHandler:
@@ -11,46 +14,50 @@ class FileHandler:
 
     @classmethod
     @property
+    @exception_handler
     def base_path(cls):
         cls._base_path = cls.find_base_dir()
         return cls._base_path
 
     @classmethod
     @property
+    @exception_handler
     def working_directory(cls):
         if not cls._working_directory:
             cls._working_directory = os.getcwd()
         return cls._working_directory
 
     @classmethod
+    @exception_handler
     def create_dir(cls, path):
         os.mkdir(path)
         message = f"A new folder {os.path.basename(os.path.normpath(path))} was created in the path: {path}"
         Logger.log_to(level=logging.INFO, message=message)
 
     @classmethod
+    @exception_handler
     def find_base_dir(cls):
         for root, dirs, files in os.walk(cls.working_directory, topdown=False):
             for name in dirs:
                 if name == ".wit":
                     return os.path.join(root, name)
-        else:
-            Logger.log_to(level=logging.ERROR, message="There is no .wit directory in project")
-            return None
-            # TODO raise excption init wit before
+        raise WitException("There is no .wit directory in project")
 
-    @exception_handler
     @classmethod
+    @exception_handler
     def get_full_path(cls, routing_end, routing_start=None):
         if not routing_start:
+            print(1)
             routing_start = cls.working_directory
         full_path = os.path.join(routing_start, routing_end)
         if not os.path.exists(full_path):
+            print(2)
             message = f"No such path exists: {full_path}"
             raise FileExistsError(message)
         return full_path
 
     @classmethod
+    @exception_handler
     def copy_item(cls, origin, target):
         if os.path.isfile(origin):
             shutil.copy(origin, target)
@@ -59,10 +66,3 @@ class FileHandler:
 
             shutil.copytree(origin, target)
         Logger.log_to(level=logging.INFO, message=f"An item has been copied from {origin} to {target}")
-        # except FileExistsError as fee:
-        #     Logger.log_to(level=logging.ERROR, message=fee)
-        # except Exception as e:
-        #     Logger.log_to(level=logging.ERROR, message=e)
-
-
-FileHandler.get_full_path("g\\", "y\\j")
