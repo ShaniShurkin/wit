dc40c9af358d3d6e33b2a639f1a1acdb8854e7e2
ShaniShurkin <shanishurkin@gmail.com>
2023-07-13 11:06:54 +0300

    decorator to push

--- exception_handler.py
+++ exception_handler.py
@@ -2,17 +2,20 @@ import logging
 import os
 from functools import partial
 from logger import Logger
+from wit_exception import WitException
 
 
 def exception_handler(func):
-    err_logger = partial(Logger.log_to, level=logging.ERROR, traceback=True)
-
+    err_logger = partial(Logger.log_to, level=logging.ERROR)
+    #, traceback = True
     def inner_function(*args, **kwargs):
         try:
             func_res = func(*args, **kwargs)
             return func_res
         except FileExistsError as ffe:
             err_logger(str(ffe))
+        except WitException as we:
+            err_logger(str(we))
         except Exception as e:
             err_logger(str(e))
 
--- wit.py
+++ wit.py
@@ -1,48 +1,48 @@
 import logging
 import os
+from functools import partial
 
+from exception_handler import exception_handler
 from files_handler import FileHandler
 from logger import Logger
+from wit_exception import WitException
+
 
 class Wit:
     _wit_dir = None
-
+    _info_logger = partial(Logger.log_to, level=logging.INFO)
     @classmethod
     def validate_is_wit_repo(cls):
         return cls._wit_dir is not None
 
     @classmethod
+    @exception_handler
     def init(cls):
-        if cls.validate_is_wit_repo():
-            pass
-            # TODO: message this repository had already init
-
-    @staticmethod
-    def init():
-        print(Wit.validate_is_wit_repo())
-        if not Wit.validate_is_wit_repo() is None:
-            raise FileExistsError("this repository had already init")
+        if not cls.validate_is_wit_repo() is None:
+            raise WitException("this repository had already init")
         else:
             FileHandler.create_dir("./.wit")
             FileHandler.create_dir("./.wit/images")
             FileHandler.create_dir("./.wit/staging_area")
             message = f"init successful"
-            Logger.log_to(level=logging.INFO, message=message)
+            cls._info_logger(message)
             cls._wit_dir = FileHandler.base_path
 
     @classmethod
+    @exception_handler
     def move_to_staging(cls, source_path):
         if not cls.validate_is_wit_repo():
             cls._wit_dir = FileHandler.base_path
         target_path = FileHandler.get_full_path("staging_area", cls._wit_dir)
         FileHandler.copy_item(source_path, target_path)
 
-    @staticmethod
-    def add(args):
+    @classmethod
+    @exception_handler
+    def add(cls, args):
         full_path = FileHandler.get_full_path(args[0])
         Wit.move_to_staging(full_path)
         message = f"add successful"
-        Logger.log_to(level=logging.INFO, message=message)
+        cls._info_logger(message)
 
     @staticmethod
     def commit():
